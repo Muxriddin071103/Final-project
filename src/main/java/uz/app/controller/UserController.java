560c59967b4ed90e0a70d983cb83551f7e53a22e
webuza <webuza101@gmail.com>
2024-11-05 14:56:38 +0500

    util created

--- src/main/java/uz/app/controller/UserController.java
+++ src/main/java/uz/app/controller/UserController.java
@@ -1,15 +1,16 @@
 package uz.app.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.app.entity.Article;
 import uz.app.entity.User;
 import uz.app.service.ArticleService;
 import uz.app.service.UserService;
+import uz.app.util.UserUtil;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -18,33 +19,43 @@ public class UserController {
 
     private final UserService userService;
     private final ArticleService articleService;
+    private final UserUtil userUtil;
 
-    @PutMapping("/{id}")
-    public ResponseEntity<User> updateUserProfile(@PathVariable Long id, @RequestBody User updatedUser) {
-        return userService.findUserById(id)
-                .map(user -> {
-                    user.setUsername(updatedUser.getUsername());
-                    user.setPassword(updatedUser.getPassword());
-                    user.setEmail(updatedUser.getEmail());
-                    user.setEnabled(updatedUser.isEnabled());
-                    User savedUser = userService.save(user);
-                    return ResponseEntity.ok(savedUser);
-                })
-                .orElse(ResponseEntity.notFound().build());
+    @PutMapping("/profile")
+    public ResponseEntity<User> updateUserProfile(@RequestBody User updatedUser) {
+        Optional<User> currentUser = userUtil.getCurrentUser();
+        if (currentUser.isPresent()) {
+            currentUser.get().setUsername(updatedUser.getUsername());
+            currentUser.get().setPassword(updatedUser.getPassword());
+            currentUser.get().setEmail(updatedUser.getEmail());
+            currentUser.get().setEnabled(updatedUser.isEnabled());
+            User savedUser = userService.save(currentUser.orElse(null));
+            return ResponseEntity.ok(savedUser);
+        }
+        return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/{id}/articles")
-    public ResponseEntity<List<Article>> getUserArticles(@PathVariable Long id) {
-        return userService.findUserById(id)
-                .map(user -> ResponseEntity.ok(articleService.findArticlesByAuthor(user)))
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping("/profile/articles")
+    public ResponseEntity<List<Article>> getUserArticles() {
+        Optional<User> currentUser = userUtil.getCurrentUser();
+        if (currentUser.isPresent()) {
+            return ResponseEntity.ok(articleService.findArticlesByAuthor(currentUser.orElse(null)));
+        }
+        return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        return userService.findUserById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping("/profile")
+    public ResponseEntity<Optional<User>> getUserById() {
+        Optional<User> currentUser = userUtil.getCurrentUser();
+        if (currentUser.isPresent()) {
+            return ResponseEntity.ok(currentUser);
+        }
+        return ResponseEntity.notFound().build();
     }
+
 }
+
+
+
+
 
